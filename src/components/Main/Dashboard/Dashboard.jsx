7937dde595babd12277190ab857ab3ce5e500c0b
nodirchik6903 <nu2913634@gmail.com>
2024-05-14 23:17:36 +0500

    edit dashboard

--- src/components/Main/Dashboard/Dashboard.jsx
+++ src/components/Main/Dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div className='dashboard-container'>
-        <h1>Uzloyal AdminPaneliga Xush Kelibsiz</h1>
+        <h1 className='text-2xl font-semibold text-red-500'>Uzloyal AdminPaneliga Xush Kelibsiz!!!</h1>
     </div>
   )
 }
