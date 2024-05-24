6f545364a88873b538c093a529de7e1548740e8e
nodirchik6903 <nu2913634@gmail.com>
2024-05-24 17:07:55 +0500

    news added component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,33 +1,10 @@
-<<<<<<< HEAD
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Categories from "./Categories/Categories";
-import Blogs from "./Blogs/Blogs";
-import Sources from "./Sources/Sources";
-import Dashboard from "./Dashboard/Dashboard";
-import { FaqsRoutes } from "./Faqs";
-import { ServicesRoutes } from "./Services";
-
-const Main = () => {
-  return (
-    <div className="main-container w-[85%] absolute top-[100px] p-[10px] right-0">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/sources" element={<Sources />} />
-        <Route path="/faqs/*" element={<FaqsRoutes />} />
-        <Route path="/services/*" element={<ServicesRoutes />} />
-      </Routes>
-=======
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Categories from './Categories/Categories';
 import Blogs from './Blogs/Blogs';
 import Sources from './Sources/Sources';
 import Dashboard from './Dashboard/Dashboard';
-import News from './News/News';
-
+import News from "./News/News"
 const Main = () => {
   return (
     <div className='main-container w-[85%] absolute top-[100px] p-[10px] right-0'>
@@ -38,7 +15,6 @@ const Main = () => {
             <Route path="/sources" element={<Sources />} />
             <Route path="/news" element={<News />} />
         </Routes>
->>>>>>> 9c6c20e80f9291bdd9c3347cffa96bccf97ad18c
     </div>
   );
 };
